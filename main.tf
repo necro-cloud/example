6bd236bbabf37e8edec5b0c95b1402cf5af4db03
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2025-08-08 10:42:05 +0530

    feat: garage only configuration with proper configuration passed

--- main.tf
+++ main.tf
@@ -9,62 +9,83 @@ module "cluster-issuer" {
 }
 
 module "garage" {
-  source              = "git::https://github.com/necro-cloud/modules//modules/garage?ref=feature/23/garage"
-  cloudflare_token    = var.cloudflare_token
-  cloudflare_email    = var.cloudflare_email
-  domain              = var.domain
-  cluster_issuer_name = module.cluster-issuer.cluster-issuer-name
-  configuration_file  = "${path.module}/configurator.json"
-}
-
-module "minio" {
-  source              = "git::https://github.com/necro-cloud/modules//modules/minio?ref=main"
-  cloudflare_token    = var.cloudflare_token
-  cloudflare_email    = var.cloudflare_email
-  domain              = var.domain
-  cluster_issuer_name = module.cluster-issuer.cluster-issuer-name
-  operator_namespace  = module.helm.minio-operator-namespace
-  users               = ["cloud"]
-  buckets             = ["cloud"]
-}
-
-module "cnpg" {
-  source                            = "git::https://github.com/necro-cloud/modules//modules/cnpg?ref=feature/23/garage"
-  minio_certificate_authority       = module.minio.certificate-authority-name
-  minio_namespace                   = module.minio.namespace
-  cluster_issuer_name               = module.cluster-issuer.cluster-issuer-name
-  postgres_user_minio_configuration = module.minio.postgres-user-minio-configuration
-  backup_bucket_name                = module.minio.postgres-backup-bucket
-  clients = [
+  source                 = "git::https://github.com/necro-cloud/modules//modules/garage?ref=feature/23/garage"
+  cluster_issuer_name    = module.cluster-issuer.cluster-issuer-name
+  cloudflare_token       = var.cloudflare_token
+  cloudflare_email       = var.cloudflare_email
+  domain                 = var.domain
+  replication_namespaces = "postgres"
+  required_buckets       = ["cloud", "postgresql"]
+  required_access_keys = [
     {
-      namespace          = "cloud"
-      user               = "cloud"
-      database           = "cloud"
-      derRequired        = false
-      privateKeyEncoding = "PKCS1"
+      name         = "walbackups",
+      createBucket = false,
+      permissions = [
+        {
+          bucket = "postgresql",
+          owner  = true,
+          read   = true,
+          write  = true
+        }
+      ]
+    },
+    {
+      name         = "master",
+      createBucket = true,
+      permissions = [
+        {
+          bucket = "postgresql",
+          owner  = true,
+          read   = true,
+          write  = true
+        },
+        {
+          bucket = "cloud",
+          owner  = true,
+          read   = true,
+          write  = true
+      }]
     }
   ]
-
-  depends_on = [module.minio, module.garage]
 }
 
-module "keycloak" {
-  source                                     = "git::https://github.com/necro-cloud/modules//modules/keycloak?ref=main"
-  cluster_issuer_name                        = module.cluster-issuer.cluster-issuer-name
-  postgres_namespace                         = module.cnpg.namespace
-  database_server_certificate_authority_name = module.cnpg.server-certificate-authority
-  database_client_certificate_name           = "postgresql-keycloak-client-certificate"
-  cloudflare_token                           = var.cloudflare_token
-  cloudflare_email                           = var.cloudflare_email
-  domain                                     = var.domain
-  database_credentials                       = "credentials-keycloak"
-  realm_settings                             = local.keycloak_realm_settings
+# module "cnpg" {
+#   source                            = "git::https://github.com/necro-cloud/modules//modules/cnpg?ref=feature/23/garage"
+#   minio_certificate_authority       = module.minio.certificate-authority-name
+#   minio_namespace                   = module.minio.namespace
+#   cluster_issuer_name               = module.cluster-issuer.cluster-issuer-name
+#   postgres_user_minio_configuration = module.minio.postgres-user-minio-configuration
+#   backup_bucket_name                = module.minio.postgres-backup-bucket
+#   clients = [
+#     {
+#       namespace          = "cloud"
+#       user               = "cloud"
+#       database           = "cloud"
+#       derRequired        = false
+#       privateKeyEncoding = "PKCS1"
+#     }
+#   ]
 
-  depends_on = [module.cnpg]
-}
+#   depends_on = [module.minio, module.garage]
+# }
 
-module "valkey" {
-  source                 = "git::https://github.com/necro-cloud/modules//modules/valkey?ref=main"
-  cluster_issuer_name    = module.cluster-issuer.cluster-issuer-name
-  replication_namespaces = "cloud"
-}
+# module "keycloak" {
+#   source                                     = "git::https://github.com/necro-cloud/modules//modules/keycloak?ref=main"
+#   cluster_issuer_name                        = module.cluster-issuer.cluster-issuer-name
+#   postgres_namespace                         = module.cnpg.namespace
+#   database_server_certificate_authority_name = module.cnpg.server-certificate-authority
+#   database_client_certificate_name           = "postgresql-keycloak-client-certificate"
+#   cloudflare_token                           = var.cloudflare_token
+#   cloudflare_email                           = var.cloudflare_email
+#   domain                                     = var.domain
+#   database_credentials                       = "credentials-keycloak"
+#   realm_settings                             = local.keycloak_realm_settings
+
+#   depends_on = [module.cnpg]
+# }
+
+# module "valkey" {
+#   source                 = "git::https://github.com/necro-cloud/modules//modules/valkey?ref=main"
+#   cluster_issuer_name    = module.cluster-issuer.cluster-issuer-name
+#   replication_namespaces = "cloud"
+# }
